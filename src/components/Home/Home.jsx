8d3592d72116f61376cfb3e2a506ef3cbce50539
himel <himelku1@gmail.com>
2023-11-03 14:28:30 +0600

    Home page dessign completed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,7 +3,9 @@ function Home() {
     <div className="w-full p-[50px] min-h-screen bg-slate-200">
       <div className="bg-white rounded-lg shadow-lg ">
         <div>
-          <h1 className="font-bold border-b-2 px-[35px] py-[15px] text-gray-700 text-[18px]">Gallery</h1>
+          <h1 className="font-bold border-b-2 px-[35px] py-[15px] text-gray-700 text-[18px]">
+            Gallery
+          </h1>
         </div>
         <div className="grid gap-[15px] p-[30px] grid-cols-[repeat(auto-fit,minmax(130px,1fr))]">
           <div className="border-2 rounded-lg">
